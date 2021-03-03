84f17e489d75001d6830778a9ef457be06eeedf6
Nikolas Gabriel <nikolass@ciandt.com>
2021-03-03 10:07:58 -0300

    correcting exercise 7

--- PHP Part 1/exerc7.php
+++ PHP Part 1/exerc7.php
@@ -1,8 +1,8 @@
 <?php
     function checkTriangle($l1, $l2, $l3)
     {
-        if((($l1 + $l2) > $l3 || ($l2 + $l3) > $l1 || ($l1 + $l3) > $l2) && 
-        $l1 > 0 && $l2 > 0 && $l3 > 0){
+        if((abs($l2 - $l3) < $l1 && $l1 < $l2 + $l3) && (abs($l1 - $l3) < $l2 && $l2 < $l1 + $l3)
+        && (abs($l1 - $l2) < $l3 && $l3 < $l1 + $l2) && ($l1 > 0 && $l2 > 0 && $l3 > 0) ) {
             if($l1 == $l2 && $l2 == $l3){
                 return "Triângulo equilátero";
             }
@@ -18,5 +18,5 @@
         }
     }
 
-    echo checkTriangle(5, 5, 5);
+    echo checkTriangle(1, 10, 1);
 ?>
