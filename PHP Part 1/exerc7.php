97b4e692e8d4fa35670be248106eb164fffe9811
Nikolas Gabriel <nikolass@ciandt.com>
2021-03-03 10:20:44 -0300

    taking out redundancy from exercise 7

--- PHP Part 1/exerc7.php
+++ PHP Part 1/exerc7.php
@@ -2,7 +2,7 @@
     function checkTriangle($l1, $l2, $l3)
     {
         if((abs($l2 - $l3) < $l1 && $l1 < $l2 + $l3) && (abs($l1 - $l3) < $l2 && $l2 < $l1 + $l3)
-        && (abs($l1 - $l2) < $l3 && $l3 < $l1 + $l2) && ($l1 > 0 && $l2 > 0 && $l3 > 0) ) {
+        && (abs($l1 - $l2) < $l3 && $l3 < $l1 + $l2)) {
             if($l1 == $l2 && $l2 == $l3){
                 return "Triângulo equilátero";
             }
